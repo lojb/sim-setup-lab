941f18f8d857f22a6ba8474d461b245d9af2e9da
lojb <balazsloj02@gmail.com>
2023-08-03 17:04:49 +0200

    Annotate Aero class with Getter

--- backend/src/main/java/com/simsetuplab/backend/model/setup/setupvalues/Aero.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setup/setupvalues/Aero.java
@@ -8,9 +8,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "setup_aero")
+@Getter
 public class Aero {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
