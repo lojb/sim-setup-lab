ff7fef792d37e416a66ac5bf0ff395d7dfeb768a
lojb <balazsloj02@gmail.com>
2023-08-03 17:03:47 +0200

    Create Validator entity

---
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/Validator.java
@@ -0,0 +1,41 @@
+package com.simsetuplab.backend.model.setupvalidator;
+
+import com.simsetuplab.backend.enumeration.carsetup.CarType;
+import com.simsetuplab.backend.model.setup.Setup;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Validator {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private CarType carType;
+	@OneToOne
+	private ValidateAero validateAero;
+	@OneToOne
+	private ValidateDampers validateDampers;
+	@OneToOne
+	private ValidateElectronics validateElectronics;
+	@OneToOne
+	private ValidateFuelStrategy validateFuelStrategy;
+	@OneToOne
+	private ValidateMechanicalGrip validateMechanicalGrip;
+	@OneToOne
+	private ValidateTyres validateTyres;
+
+	public boolean validate(Setup setup) {
+		boolean result = true;
+		result = result && validateAero.validate(setup.getAero());
+
+		return result;
+	}
+}
