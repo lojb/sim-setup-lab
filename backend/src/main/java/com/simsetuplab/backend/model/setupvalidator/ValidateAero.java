cd5c89c661a0a02507df3957ece70b508c34cca5
lojb <balazsloj02@gmail.com>
2023-08-03 17:05:49 +0200

    Implement validate method

--- backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateAero.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateAero.java
@@ -1,13 +1,16 @@
 package com.simsetuplab.backend.model.setupvalidator;
 
 import com.simsetuplab.backend.enumeration.carsetup.CarType;
+import com.simsetuplab.backend.model.setup.setupvalues.Aero;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class ValidateAero {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +28,41 @@ public class ValidateAero {
 	private int frontBrakeDuctsMax;
 	private int rearBrakeDuctsMin;
 	private int rearBrakeDuctsMax;
+
+	public boolean validate(Aero aero) {
+		boolean result = true;
+
+		result = result && validateFrontRideHeight(aero.getFrontRideHeight());
+		result = result && validateRearRideHeight(aero.getRearRideHeight());
+		result = result && validateSplitter(aero.getSplitter());
+		result = result && validateRearWing(aero.getRearWing());
+		result = result && validateFrontBrakeDucts(aero.getFrontBrakeDucts());
+		result = result && validateRearBrakeDucts(aero.getRearBrakeDucts());
+
+		return result;
+	}
+
+	private boolean validateFrontRideHeight(int frontRideHeight) {
+		return frontRideHeight >= frontRideHeightMin && frontRideHeight <= frontRideHeightMax;
+	}
+
+	private boolean validateRearRideHeight(int rearRideHeight) {
+		return rearRideHeight >= rearRideHeightMin && rearRideHeight <= rearRideHeightMax;
+	}
+
+	private boolean validateSplitter(int splitter) {
+		return splitter >= splitterMin && splitter <= splitterMax;
+	}
+
+	private boolean validateRearWing(int rearWing) {
+		return rearWing >= rearWingMin && rearWing <= rearWingMax;
+	}
+
+	private boolean validateFrontBrakeDucts(int frontBrakeDucts) {
+		return frontBrakeDucts >= frontBrakeDuctsMin && frontBrakeDucts <= frontBrakeDuctsMax;
+	}
+
+	private boolean validateRearBrakeDucts(int rearBrakeDucts) {
+		return rearBrakeDucts >= rearBrakeDuctsMin && rearBrakeDucts <= rearBrakeDuctsMax;
+	}
 }
