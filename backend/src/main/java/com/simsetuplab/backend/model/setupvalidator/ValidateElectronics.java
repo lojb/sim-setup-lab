84f3ad15766cca4c2de124d18663f4b2cc7cb2b7
lojb <balazsloj02@gmail.com>
2023-08-04 13:40:37 +0200

    Implement Electronics validation

--- backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateElectronics.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateElectronics.java
@@ -1,6 +1,7 @@
 package com.simsetuplab.backend.model.setupvalidator;
 
 import com.simsetuplab.backend.enumeration.carsetup.CarType;
+import com.simsetuplab.backend.model.setup.setupvalues.Electronics;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,4 +22,28 @@ public class ValidateElectronics {
 	private int ecuMapMax;
 	private int telemetryLapsMin;
 	private int telemetryLapsMax;
+
+	public boolean validate(Electronics electronics) {
+
+		return validatetractionControl(electronics.getTractionControl())
+				&& validateAbs(electronics.getAbs())
+				&& validateEcuMap(electronics.getEcuMap())
+				&& validateTelemetryLaps(electronics.getTelemetryLaps());
+	}
+
+	private boolean validatetractionControl(int tractionControl) {
+		return tractionControl >= tractionControlMin && tractionControl <= tractionControlMax;
+	}
+
+	private boolean validateAbs(int abs) {
+		return abs >= absMin && abs <= absMax;
+	}
+
+	private boolean validateEcuMap(int ecuMap) {
+		return ecuMap >= ecuMapMin && ecuMap <= ecuMapMax;
+	}
+
+	private boolean validateTelemetryLaps(int telemetryLaps) {
+		return telemetryLaps >= telemetryLapsMin && telemetryLaps <= telemetryLapsMax;
+	}
 }
