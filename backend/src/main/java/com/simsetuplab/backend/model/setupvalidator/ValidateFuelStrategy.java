4fd7accf0a8de1e628b099df319106b4b8e6a0a1
lojb <balazsloj02@gmail.com>
2023-08-04 13:40:53 +0200

    Implement FuelStrategy validation

--- backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateFuelStrategy.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateFuelStrategy.java
@@ -2,6 +2,7 @@ package com.simsetuplab.backend.model.setupvalidator;
 
 import com.simsetuplab.backend.enumeration.carsetup.CarType;
 import com.simsetuplab.backend.enumeration.carsetup.TyreType;
+import com.simsetuplab.backend.model.setup.setupvalues.FuelStrategy;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,4 +21,18 @@ public class ValidateFuelStrategy {
 	private int brakesMin = 1;
 	private int brakesMax = 4;
 	private double fuelPerLap;
+
+	public boolean validate(FuelStrategy fuelStrategy) {
+		return validateFuel(fuelStrategy.getFuel())
+				&& validateBrakes(fuelStrategy.getFrontBrakes())
+				&& validateBrakes(fuelStrategy.getRearBrakes());
+	}
+
+	private boolean validateFuel(int fuel) {
+		return fuel >= fuelMin && fuel <= fuelMax;
+	}
+
+	private boolean validateBrakes(int brakes) {
+		return brakes >= brakesMin && brakes <= brakesMax;
+	}
 }
