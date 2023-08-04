852bb38c45917188b8fe2ca608018bb212439735
lojb <balazsloj02@gmail.com>
2023-08-04 13:41:07 +0200

    Implement MechanicalGrip validation

--- backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateMechanicalGrip.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateMechanicalGrip.java
@@ -1,6 +1,7 @@
 package com.simsetuplab.backend.model.setupvalidator;
 
 import com.simsetuplab.backend.enumeration.carsetup.CarType;
+import com.simsetuplab.backend.model.setup.setupvalues.MechanicalGrip;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -36,4 +37,63 @@ public class ValidateMechanicalGrip {
 	private int rearBumpStopRangeMax;
 	private int preloadDifferentialMin;
 	private int preloadDifferentialMax;
+
+	public boolean validate(MechanicalGrip mechanicalGrip) {
+		return validateFrontAntiRollBar(mechanicalGrip.getFrontAntiRollBar())
+				&& validateRearAntiRollBar(mechanicalGrip.getRearAntiRollBar())
+				&& validateBrakeBias(mechanicalGrip.getBrakeBias())
+				&& validateSteerRatio(mechanicalGrip.getSteerRatio())
+				&& validateFrontWheelRate(mechanicalGrip.getFrontWheelRate())
+				&& validateRearWheelRate(mechanicalGrip.getRearWheelRate())
+				&& validateFrontBumpStopRate(mechanicalGrip.getFrontBumpstopRate())
+				&& validateRearBumpStopRate(mechanicalGrip.getRearBumpstopRate())
+				&& validateFrontBumpStopRange(mechanicalGrip.getFrontBumpstopRange())
+				&& validateRearBumpStopRange(mechanicalGrip.getRearBumpstopRange())
+				&& validatePreloadDifferential(mechanicalGrip.getPreloadDifferential());
+
+	}
+
+	private boolean validateFrontAntiRollBar(int frontAntiRollBar) {
+		return frontAntiRollBar >= frontAntiRollBarMin && frontAntiRollBar <= frontAntiRollBarMax;
+	}
+
+	private boolean validateRearAntiRollBar(int rearAntiRollBar) {
+		return rearAntiRollBar >= rearAntiRollBarMin && rearAntiRollBar <= rearAntiRollBarMax;
+	}
+
+	private boolean validateBrakeBias(float brakeBias) {
+		return brakeBias >= brakeBiasMin && brakeBias <= brakeBiasMax;
+	}
+
+	private boolean validateSteerRatio(int steerRatio) {
+		return steerRatio >= steerRatioMin && steerRatio <= steerRatioMax;
+	}
+
+	private boolean validateFrontWheelRate(int frontWheelRate) {
+		return frontWheelRate >= frontWheelRateMin && frontWheelRate <= frontWheelRateMax;
+	}
+
+	private boolean validateRearWheelRate(int rearWheelRate) {
+		return rearWheelRate >= rearWheelRateMin && rearWheelRate <= rearWheelRateMax;
+	}
+
+	private boolean validateFrontBumpStopRate(int frontBumpStopRate) {
+		return frontBumpStopRate >= frontBumpStopRateMin && frontBumpStopRate <= frontBumpStopRateMax;
+	}
+
+	private boolean validateRearBumpStopRate(int rearBumpStopRate) {
+		return rearBumpStopRate >= rearBumpStopRateMin && rearBumpStopRate <= rearBumpStopRateMax;
+	}
+
+	private boolean validateFrontBumpStopRange(int frontBumpStopRange) {
+		return frontBumpStopRange >= frontBumpStopRangeMin && frontBumpStopRange <= frontBumpStopRangeMax;
+	}
+
+	private boolean validateRearBumpStopRange(int rearBumpStopRange) {
+		return rearBumpStopRange >= rearBumpStopRangeMin && rearBumpStopRange <= rearBumpStopRangeMax;
+	}
+
+	private boolean validatePreloadDifferential(int preloadDifferential) {
+		return preloadDifferential >= preloadDifferentialMin && preloadDifferential <= preloadDifferentialMax;
+	}
 }
