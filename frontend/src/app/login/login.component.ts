3766c28b266f131982275f70ca318ccd02d46119
lojb <balazsloj02@gmail.com>
2023-08-10 16:08:56 +0200

    Implement frontend validation to Login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
+    this.LoginUser = this.LoginUser.bind(this);
+  }
+
+  LoginUser() {
+    console.log(this.loginForm.value)
+  }
+
+  get Username(): FormControl {
+    return this.loginForm.get("username") as FormControl
+  }
+  get Password(): FormControl {
+    return this.loginForm.get("password") as FormControl
+  }
 
 }
