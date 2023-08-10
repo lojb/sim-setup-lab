5f1181ecc7b9f4653e03947d5475888668132233
lojb <balazsloj02@gmail.com>
2023-08-10 08:45:37 +0200

    Set up Angular app

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {NgOptimizedImage} from "@angular/common";
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
