6952db00d5b0bed5a7b79855c7d7cbba60e2491c
lojb <balazsloj02@gmail.com>
2023-08-10 17:30:31 +0200

    Convert username and email constraints to be unique

--- backend/src/main/java/com/simsetuplab/backend/model/user/User.java
+++ backend/src/main/java/com/simsetuplab/backend/model/user/User.java
@@ -1,5 +1,6 @@
 package com.simsetuplab.backend.model.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +17,9 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(unique = true)
 	private String username;
 	private String password;
+	@Column(unique = true)
 	private String email;
 }
