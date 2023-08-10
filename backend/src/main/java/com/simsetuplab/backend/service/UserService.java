af27b45ce267a6f0009f146a9e440348626daf89
lojb <balazsloj02@gmail.com>
2023-08-10 16:04:21 +0200

    Add password encoding to user registration

--- backend/src/main/java/com/simsetuplab/backend/service/UserService.java
+++ backend/src/main/java/com/simsetuplab/backend/service/UserService.java
@@ -2,6 +2,7 @@ package com.simsetuplab.backend.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.simsetuplab.backend.model.user.User;
 import com.simsetuplab.backend.repository.UserRepository;
@@ -9,10 +10,12 @@ import com.simsetuplab.backend.repository.UserRepository;
 @Service
 public class UserService {
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Autowired
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	public List<User> getAllUsers() {
@@ -24,10 +27,15 @@ public class UserService {
 	}
 
 	public User addOrUpdateUser(User user) {
+		String encryptedPassword = passwordEncoder.encode(user.getPassword());
+
+		user.setPassword(encryptedPassword);
+
 		return userRepository.save(user);
 	}
 
 	public void deleteUser(User user) {
 		userRepository.delete(user);
 	}
+
 }
