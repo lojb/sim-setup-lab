8649f76a9a6b5596de66c5c8fc17030516112c6f
lojb <balazsloj02@gmail.com>
2023-08-17 14:36:45 +0200

    Set user infos in localStorage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {SocialAuthService} from "@abacritt/angularx-social-login";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -12,14 +13,30 @@ export class AppComponent {
   user: any;
   loggedIn: any;
 
-  constructor(private authService: SocialAuthService) {
+  constructor(private authService: SocialAuthService, private http: HttpClient) {
   }
 
   ngOnInit() {
+    localStorage.setItem('serverUrl', 'http://localhost:8080')
     this.authService.authState.subscribe((user) => {
       this.user = user;
       this.loggedIn = (user != null);
-      console.log(this.user);
+      localStorage.setItem('idToken', JSON.stringify(user.idToken));
+      localStorage.setItem('email', JSON.stringify(user.email));
+      localStorage.setItem('name', JSON.stringify(user.name));
+      localStorage.setItem('provider', JSON.stringify(user.provider));
+      localStorage.setItem('photoUrl', JSON.stringify(user.photoUrl));
+
+      /*const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      const body = localStorage.getItem('idToken');
+      const url = localStorage.getItem('serverUrl') + '/login';
+
+      this.http.post('http://localhost:8080/api/auth/authenticate', body, { headers })
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+          }
+        );*/
     });
   }
 }
