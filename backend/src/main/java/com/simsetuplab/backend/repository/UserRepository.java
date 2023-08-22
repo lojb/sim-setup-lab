c214d4cbfa1b11746ed979059ac855a7b3fd0d16
lojb <balazsloj02@gmail.com>
2023-08-22 13:52:49 +0200

    Update UserRepository

--- backend/src/main/java/com/simsetuplab/backend/repository/UserRepository.java
+++ backend/src/main/java/com/simsetuplab/backend/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.simsetuplab.backend.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.simsetuplab.backend.model.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	User findUserById(Long id);
+	Optional<User> findUserById(Long id);
 
-	User findUserByUsername(String username);
+	Optional<User> findUserByUsername(String username);
 
-	User findUserByEmail(String email);
+	Optional<User> findUserByEmail(String email);
 }
