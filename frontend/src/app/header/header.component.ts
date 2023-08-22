afc47e69098e2a1ce393580963ef6ac62c930828
lojb <balazsloj02@gmail.com>
2023-08-22 13:57:49 +0200

    Update header component

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private auth: AuthService) {
+
+  }
+
+  LogoutUser() {
+    this.auth.logout();
+  }
+
+  protected readonly localStorage = localStorage;
 }
