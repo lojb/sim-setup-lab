cb2ef53ff21f9eb3d8642a25fdf8e9ff91eb3347
lojb <balazsloj02@gmail.com>
2023-08-22 13:51:11 +0200

    Update SecurityConfig

--- backend/src/main/java/com/simsetuplab/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/simsetuplab/backend/config/SecurityConfig.java
@@ -2,25 +2,35 @@ package com.simsetuplab.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private final JwtAuthenticationFilter jwtAuthFilter;
+	private final AuthenticationProvider authenticationProvider;
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-				.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(auth -> {
-					auth.requestMatchers("/").permitAll();
-					//auth.anyRequest().authenticated();
-					auth.anyRequest().permitAll();
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(request -> {
+					request.requestMatchers("/", "/api/v1/auth/**", "/login").permitAll();
+					request.anyRequest().authenticated();
+					//request.anyRequest().permitAll();
 				})
-				.oauth2Login(Customizer.withDefaults())
-				.formLogin(Customizer.withDefaults())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 }
