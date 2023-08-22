3dfdef4d33cbf794823b134e0f8f99252589e0bc
lojb <balazsloj02@gmail.com>
2023-08-22 13:53:23 +0200

    Update UserController

--- backend/src/main/java/com/simsetuplab/backend/controller/UserController.java
+++ backend/src/main/java/com/simsetuplab/backend/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,14 +34,14 @@ public class UserController {
 		return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/id/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
 		return ResponseEntity.ok(userService.getUserById(id));
 	}
 
-	@PostMapping
-	public ResponseEntity<User> addUser(@RequestBody User user) {
-		return ResponseEntity.ok(userService.addOrUpdateUser(user));
+	@GetMapping("/user/{username}")
+	public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
+		return ResponseEntity.ok(userService.getUserByUsername(username));
 	}
 
 	@PutMapping
