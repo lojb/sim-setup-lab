4934ad1cfc52075c492ead2955b4c8ccc7a16403
lojb <balazsloj02@gmail.com>
2023-08-28 17:08:33 +0200

    Implement Electronics setup component

---
+++ frontend/src/app/setup/electronics/electronics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {SetupPart} from "../setup-part";
+
+@Component({
+  selector: 'app-electronics',
+  templateUrl: './electronics.component.html',
+  styleUrls: ['./electronics.component.css']
+})
+export class ElectronicsComponent implements SetupPart{
+
+}
