08aa89689590a8414e314ea9626a88c2b8100349
lojb <balazsloj02@gmail.com>
2023-08-30 13:08:31 +0200

    Bind SetupValues data with Aero component data

--- frontend/src/app/setup/aero/aero.component.ts
+++ frontend/src/app/setup/aero/aero.component.ts
@@ -1,29 +1,12 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Aero} from "../../model/aero";
 
 @Component({
   selector: 'app-aero',
   templateUrl: './aero.component.html',
   styleUrls: ['./aero.component.css']
 })
-export class AeroComponent{
-  @Output() dataEmitter = new EventEmitter<any>();
+export class AeroComponent {
 
-  frontRideHeight: any;
-  rearRideHeight: any;
-  splitter: any;
-  rearWing: any;
-  frontBrakeDucts: any;
-  rearBrakeDucts: any;
-
-  sendData() {
-    const aeroValues = {
-      frontRideHeight: this.frontRideHeight,
-      rearRideHeight: this.rearRideHeight,
-      splitter: this.splitter,
-      rearWing: this.rearWing,
-      frontBrakeDucts: this.frontBrakeDucts,
-      rearBrakeDucts: this.rearBrakeDucts
-    };
-    this.dataEmitter.emit(aeroValues);
-  }
+  @Input() aeroValues: Aero;
 }
