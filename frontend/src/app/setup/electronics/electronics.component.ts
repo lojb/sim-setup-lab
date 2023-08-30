176d48456ae9b956b6170f27bc5e2c7289445f1e
lojb <balazsloj02@gmail.com>
2023-08-30 13:08:47 +0200

    Bind SetupValues data with Electronics component data

--- frontend/src/app/setup/electronics/electronics.component.ts
+++ frontend/src/app/setup/electronics/electronics.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Electronics} from "../../model/electronics";
 
 @Component({
   selector: 'app-electronics',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./electronics.component.css']
 })
 export class ElectronicsComponent{
-
+  @Input() electronicsValues: Electronics;
 }
