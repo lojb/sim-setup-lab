f7a4664daebe2af5463aa6293d4b9e306d219e77
lojb <balazsloj02@gmail.com>
2023-08-30 13:08:40 +0200

    Bind SetupValues data with Dampers component data

--- frontend/src/app/setup/dampers/dampers.component.ts
+++ frontend/src/app/setup/dampers/dampers.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Dampers} from "../../model/dampers";
 
 @Component({
   selector: 'app-dampers',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class DampersComponent{
 
+  @Input() dampersValues: Dampers;
 }
