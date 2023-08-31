2b75b5646cc9e55985802858b8534b9f4992a923
lojb <balazsloj02@gmail.com>
2023-08-31 15:58:10 +0200

    Add user to setup object

--- frontend/src/app/setup/setup.component.ts
+++ frontend/src/app/setup/setup.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-import {SetupValues} from "../model/setup-values";
+import {SetupValues} from "../model/setup/setup-values";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment.development";
 
 @Component({
   selector: 'app-setup',
@@ -11,13 +13,26 @@ export class SetupComponent {
   selectedSetupPart: string | undefined;
   setupValues = new SetupValues();
 
+  constructor(private http: HttpClient) {
 
+  }
 
   ToggleSelectedPart(setupPart: string) {
     this.selectedSetupPart = setupPart;
   }
 
   SubmitSetup() {
+    this.setupValues.user = localStorage.getItem('userId');
     console.log(this.setupValues)
+
+    /*const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+      });
+
+    this.http.post(`${environment.backend}/api/v1/setup`, this.setupValues, {headers})
+      .subscribe((setup: any) => {
+        console.log(setup)
+      });*/
   }
 }
