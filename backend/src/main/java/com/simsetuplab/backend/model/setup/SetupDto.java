ded21be7d9423940b198a9d2e8ac837ae9b68309
lojb <balazsloj02@gmail.com>
2023-09-01 17:25:18 +0200

    Create SetupDto

---
+++ backend/src/main/java/com/simsetuplab/backend/model/setup/SetupDto.java
@@ -0,0 +1,25 @@
+package com.simsetuplab.backend.model.setup;
+
+import com.simsetuplab.backend.model.setup.setupvalues.Aero;
+import com.simsetuplab.backend.model.setup.setupvalues.Dampers;
+import com.simsetuplab.backend.model.setup.setupvalues.Electronics;
+import com.simsetuplab.backend.model.setup.setupvalues.FuelStrategy;
+import com.simsetuplab.backend.model.setup.setupvalues.MechanicalGrip;
+import com.simsetuplab.backend.model.setup.setupvalues.Tyres;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SetupDto {
+	private String name;
+	private long userId;
+	private String carType;
+	private Aero aero;
+	private Dampers dampers;
+	private Electronics electronics;
+	private FuelStrategy fuelStrategy;
+	private MechanicalGrip mechanicalGrip;
+	private Tyres tyres;
+}
