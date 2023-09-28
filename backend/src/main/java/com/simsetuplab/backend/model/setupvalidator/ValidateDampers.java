66047a84f2ee4355b7e95dbb99d036a1caee0f50
lojb <balazsloj02@gmail.com>
2023-09-28 10:11:14 +0200

    Implement ValidateDampers with exception handling

--- backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateDampers.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateDampers.java
@@ -1,5 +1,6 @@
 package com.simsetuplab.backend.model.setupvalidator;
 
+import com.simsetuplab.backend.exception.ApiRequestException;
 import com.simsetuplab.backend.model.setup.setupvalues.Dampers;
 
 import jakarta.persistence.Entity;
@@ -32,54 +33,72 @@ public class ValidateDampers {
 	private int rearFastReboundMax;
 
 	public boolean validate(Dampers dampers) {
-		return validateFrontBump(dampers.getFrontLeftBump(), dampers.getFrontRightBump())
-				&& validateFrontFastBump(dampers.getFrontLeftFastBump(), dampers.getFrontRightFastBump())
-				&& validateFrontRebound(dampers.getFrontLeftRebound(), dampers.getFrontRightRebound())
-				&& validateFrontFastRebound(dampers.getFrontLeftFastRebound(), dampers.getFrontRightFastRebound())
-				&& validateRearBump(dampers.getRearLeftBump(), dampers.getRearRightBump())
-				&& validateRearFastBump(dampers.getRearLeftFastBump(), dampers.getRearRightFastBump())
-				&& validateRearRebound(dampers.getRearLeftRebound(), dampers.getRearRightRebound())
-				&& validateRearFastRebound(dampers.getRearLeftFastRebound(), dampers.getRearRightFastRebound());
+		validateFrontBump(dampers.getFrontLeftBump(), dampers.getFrontRightBump());
+		validateFrontFastBump(dampers.getFrontLeftFastBump(), dampers.getFrontRightFastBump());
+		validateFrontRebound(dampers.getFrontLeftRebound(), dampers.getFrontRightRebound());
+		validateFrontFastRebound(dampers.getFrontLeftFastRebound(), dampers.getFrontRightFastRebound());
+		validateRearBump(dampers.getRearLeftBump(), dampers.getRearRightBump());
+		validateRearFastBump(dampers.getRearLeftFastBump(), dampers.getRearRightFastBump());
+		validateRearRebound(dampers.getRearLeftRebound(), dampers.getRearRightRebound());
+		validateRearFastRebound(dampers.getRearLeftFastRebound(), dampers.getRearRightFastRebound());
+
+		return true;
 	}
 
-	private boolean validateFrontBump(int frontLeftBump, int frontRightBump) {
-		return frontLeftBump >= frontBumpMin && frontLeftBump <= frontBumpMax
-				&& frontRightBump >= frontBumpMin && frontRightBump <= frontBumpMax;
+	private void validateFrontBump(int frontLeftBump, int frontRightBump) {
+		if (!(frontLeftBump >= frontBumpMin && frontLeftBump <= frontBumpMax
+				&& frontRightBump >= frontBumpMin && frontRightBump <= frontBumpMax)) {
+			throw new ApiRequestException("Invalid front bump values");
+		}
 	}
 
-	private boolean validateFrontFastBump(int frontLeftFastBump, int frontRightFastBump) {
-		return frontLeftFastBump >= frontFastBumpMin && frontLeftFastBump <= frontFastBumpMax
-				&& frontRightFastBump >= frontFastBumpMin && frontRightFastBump <= frontFastBumpMax;
+	private void validateFrontFastBump(int frontLeftFastBump, int frontRightFastBump) {
+		if (!(frontLeftFastBump >= frontFastBumpMin && frontLeftFastBump <= frontFastBumpMax
+				&& frontRightFastBump >= frontFastBumpMin && frontRightFastBump <= frontFastBumpMax)) {
+			throw new ApiRequestException("Invalid front fast bump values");
+		}
 	}
 
-	private boolean validateFrontRebound(int frontLeftRebound, int frontRightRebound) {
-		return frontLeftRebound >= frontReboundMin && frontLeftRebound <= frontReboundMax
-				&& frontRightRebound >= frontReboundMin && frontRightRebound <= frontReboundMax;
+	private void validateFrontRebound(int frontLeftRebound, int frontRightRebound) {
+		if (!(frontLeftRebound >= frontReboundMin && frontLeftRebound <= frontReboundMax
+				&& frontRightRebound >= frontReboundMin && frontRightRebound <= frontReboundMax)) {
+			throw new ApiRequestException("Invalid front rebound values");
+		}
 	}
 
-	private boolean validateFrontFastRebound(int frontLeftFastRebound, int frontRightFastRebound) {
-		return frontLeftFastRebound >= frontFastReboundMin && frontLeftFastRebound <= frontFastReboundMax
-				&& frontRightFastRebound >= frontFastReboundMin && frontRightFastRebound <= frontFastReboundMax;
+	private void validateFrontFastRebound(int frontLeftFastRebound, int frontRightFastRebound) {
+		if (!(frontLeftFastRebound >= frontFastReboundMin && frontLeftFastRebound <= frontFastReboundMax
+				&& frontRightFastRebound >= frontFastReboundMin && frontRightFastRebound <= frontFastReboundMax)) {
+			throw new ApiRequestException("Invalid front fast rebound values");
+		}
 	}
 
-	private boolean validateRearBump(int rearLeftBump, int rearRightBump) {
-		return rearLeftBump >= rearBumpMin && rearLeftBump <= rearBumpMax
-				&& rearRightBump >= rearBumpMin && rearRightBump <= rearBumpMax;
+	private void validateRearBump(int rearLeftBump, int rearRightBump) {
+		if (!(rearLeftBump >= rearBumpMin && rearLeftBump <= rearBumpMax
+				&& rearRightBump >= rearBumpMin && rearRightBump <= rearBumpMax)) {
+			throw new ApiRequestException("Invalid rear bump values");
+		}
 	}
 
-	private boolean validateRearFastBump(int rearLeftFastBump, int rearRightFastBump) {
-		return rearLeftFastBump >= rearFastBumpMin && rearLeftFastBump <= rearFastBumpMax
-				&& rearRightFastBump >= rearFastBumpMin && rearRightFastBump <= rearFastBumpMax;
+	private void validateRearFastBump(int rearLeftFastBump, int rearRightFastBump) {
+		if (!(rearLeftFastBump >= rearFastBumpMin && rearLeftFastBump <= rearFastBumpMax
+				&& rearRightFastBump >= rearFastBumpMin && rearRightFastBump <= rearFastBumpMax)) {
+			throw new ApiRequestException("Invalid rear fast bump values");
+		}
 	}
 
-	private boolean validateRearRebound(int rearLeftRebound, int rearRightRebound) {
-		return rearLeftRebound >= rearReboundMin && rearLeftRebound <= rearReboundMax
-				&& rearRightRebound >= rearReboundMin && rearRightRebound <= rearReboundMax;
+	private void validateRearRebound(int rearLeftRebound, int rearRightRebound) {
+		if (!(rearLeftRebound >= rearReboundMin && rearLeftRebound <= rearReboundMax
+				&& rearRightRebound >= rearReboundMin && rearRightRebound <= rearReboundMax)) {
+			throw new ApiRequestException("Invalid rear rebound values");
+		}
 	}
 
-	private boolean validateRearFastRebound(int rearLeftFastRebound, int rearRightFastRebound) {
-		return rearLeftFastRebound >= rearFastReboundMin && rearLeftFastRebound <= rearFastReboundMax
-				&& rearRightFastRebound >= rearFastReboundMin && rearRightFastRebound <= rearFastReboundMax;
+	private void validateRearFastRebound(int rearLeftFastRebound, int rearRightFastRebound) {
+		if (!(rearLeftFastRebound >= rearFastReboundMin && rearLeftFastRebound <= rearFastReboundMax
+				&& rearRightFastRebound >= rearFastReboundMin && rearRightFastRebound <= rearFastReboundMax)) {
+			throw new ApiRequestException("Invalid rear fast rebound values");
+		}
 	}
 
 }
