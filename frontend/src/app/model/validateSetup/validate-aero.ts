1252d35b849058ad93a6ea7a5f5b2f46440fa4f6
lojb <balazsloj02@gmail.com>
2023-09-29 15:48:06 +0200

    Add validation functions to validate-aero.ts

--- frontend/src/app/model/validateSetup/validate-aero.ts
+++ frontend/src/app/model/validateSetup/validate-aero.ts
@@ -11,4 +11,28 @@ export class ValidateAero {
   frontBrakeDuctsMax: number;
   rearBrakeDuctsMin: number;
   rearBrakeDuctsMax: number;
+
+  ValidateFrontRideHeight(frontRideHeight: number): boolean {
+    return frontRideHeight >= this.frontRideHeightMin && frontRideHeight <= this.frontRideHeightMax
+  }
+
+  ValidateRearRideHeight(rearRideHeight: number) {
+    return rearRideHeight >= this.rearRideHeightMin && rearRideHeight <= this.rearRideHeightMax
+  }
+
+  ValidateSplitter(splitter: number) {
+    return splitter >= this.splitterMin && splitter <= this.splitterMax
+  }
+
+  ValidateRearWing(rearWing: number) {
+    return rearWing >= this.rearWingMin && rearWing <= this.rearWingMax
+  }
+
+  ValidateFrontBrakeDucts(frontBrakeDucts: number) {
+    return frontBrakeDucts >= this.frontBrakeDuctsMin && frontBrakeDucts <= this.frontBrakeDuctsMax
+  }
+
+  ValidateRearBrakeDucts(rearBrakeDucts: number) {
+    return rearBrakeDucts >= this.rearBrakeDuctsMin && rearBrakeDucts <= this.rearBrakeDuctsMax
+  }
 }
