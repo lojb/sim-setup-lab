1814f13f46550563d312b3ca792f0c7e7393c4a8
lojb <balazsloj02@gmail.com>
2023-10-02 10:43:02 +0200

    Refactor data.sql to insert correct validation values

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -98,49 +98,51 @@ ON CONFLICT (id) DO NOTHING;
 
 
 INSERT INTO public.validate_electronics
-VALUES (1, 0, 0, 0, 0, 0, 0, 0, 0)
+VALUES (1, 10, 0, 3, 1, 99, 0, 11, 0)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_electronics
-VALUES (2, 0, 0, 0, 0, 0, 0, 0, 0)
+VALUES (2, 12, 0, 13, 1, 99, 0, 12, 0)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_electronics
-VALUES (3, 0, 0, 0, 0, 0, 0, 0, 0)
+VALUES (3, 11, 0, 10, 1, 99, 0, 11, 0)
 ON CONFLICT (id) DO NOTHING;
 
 
 
 INSERT INTO public.validate_fuel_strategy
-VALUES (1, 4, 1, 0, 2, 0, 0)
+VALUES (1, 4, 1, 108, 2, 0, 0)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_fuel_strategy
-VALUES (2, 4, 1, 0, 2, 0, 0)
+VALUES (2, 4, 1, 120, 2, 0, 0)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_fuel_strategy
-VALUES (3, 4, 1, 0, 2, 0, 0)
+VALUES (3, 4, 1, 120, 2, 0, 0)
 ON CONFLICT (id) DO NOTHING;
 
 
 
 INSERT INTO public.validate_mechanical_grip
-VALUES (1, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+VALUES (1, 71, 50, 100, 10, 0, 46, 0, 2500, 300, 202000, 130000, 300, 20, 13, 0, 76, 0, 2500, 300, 131000, 71000, 17,
+        11)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_mechanical_grip
-VALUES (2, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+VALUES (2, 68, 47, 100, 11, 0, 40, 0, 2500, 300, 230000, 118000, 160, 20, 14, 0, 30, 0, 2500, 300, 209000, 83000, 17,
+        11)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_mechanical_grip
-VALUES (3, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+VALUES (3, 64, 43, 100, 6, 0, 49, 0, 2500, 300, 173500, 100500, 300, 20, 6, 0, 50, 0, 2500, 300, 237000, 137000, 17, 11)
 ON CONFLICT (id) DO NOTHING;
 
 
 
 INSERT INTO public.validate_tyres
-VALUES (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 25)
+VALUES (1, 14.1, 6, -1.5, -4, 0.4, -0.4, -1, -3.5, 0.4, -0.4, 0, 0)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_tyres
-VALUES (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 25)
+VALUES (2, 16.3, 5.3, -1.5, -4, 0.48, -0.48, -1, -3.5, 0.4, -0.1, 35, 20.3)
 ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.validate_tyres
-VALUES (3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 25)
+VALUES (3, 12.4, 6.5, -1.5, -4, 0.4, -0.4, -1, -3.5, 0.4, -0.4, 35, 20.3)
 ON CONFLICT (id) DO NOTHING;
 
 
