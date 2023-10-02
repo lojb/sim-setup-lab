655743cc4f10b16a00ef1b5d0672f78a7cc1abee
lojb <balazsloj02@gmail.com>
2023-10-02 09:20:16 +0200

    Implement validation methods in validate-electronics.ts

--- frontend/src/app/model/validateSetup/validate-electronics.ts
+++ frontend/src/app/model/validateSetup/validate-electronics.ts
@@ -7,4 +7,21 @@ export class ValidateElectronics {
   ecuMapMax: number;
   telemetryLapsMin: number;
   telemetryLapsMax: number;
+
+
+  ValidateTractionControl(tractionControl: number) {
+    return tractionControl >= this.tractionControlMin && tractionControl <= this.tractionControlMax;
+  }
+
+  ValidateAbs(abs: number) {
+    return abs >= this.absMin && abs <= this.absMax;
+  }
+
+  ValidateEcuMap(ecuMap: number) {
+    return ecuMap >= this.ecuMapMin && ecuMap <= this.ecuMapMax;
+  }
+
+  ValidateTelemetryLaps(telemetryLaps: number) {
+    return telemetryLaps >= this.telemetryLapsMin && telemetryLaps <= this.telemetryLapsMax;
+  }
 }
