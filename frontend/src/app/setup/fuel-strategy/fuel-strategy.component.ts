90be9f72dbdf584e82653793e8081e9251dedf35
lojb <balazsloj02@gmail.com>
2023-10-04 16:54:57 +0200

    Create form for fuel-strategy component validation

--- frontend/src/app/setup/fuel-strategy/fuel-strategy.component.ts
+++ frontend/src/app/setup/fuel-strategy/fuel-strategy.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {FuelStrategy} from "../../model/setup/fuel-strategy";
 import {ValidateFuelStrategy} from "../../model/validateSetup/validate-fuel-strategy";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-fuel-strategy',
@@ -9,5 +10,47 @@ import {ValidateFuelStrategy} from "../../model/validateSetup/validate-fuel-stra
 })
 export class FuelStrategyComponent{
   @Input() fuelStrategyValues: FuelStrategy;
-  @Input() validateFuelStratgy: ValidateFuelStrategy;
+  @Input() validateFuelStrategy: ValidateFuelStrategy;
+  fuelStrategyForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.fuelStrategyForm = this.formBuilder.group({
+      fuel: [
+        this.fuelStrategyValues.fuel,
+        [
+          Validators.required,
+          Validators.min(this.validateFuelStrategy.fuelMin),
+          Validators.max(this.validateFuelStrategy.fuelMax),
+        ]
+      ],
+      tyreType: [
+        this.fuelStrategyValues.tyreType,
+        [
+          Validators.required
+        ]
+      ],
+      frontBrakes: [
+        this.fuelStrategyValues.frontBrakes,
+        [
+          Validators.required,
+          Validators.min(this.validateFuelStrategy.brakesMin),
+          Validators.max(this.validateFuelStrategy.brakesMax),
+        ]
+      ],
+      rearBrakes: [
+        this.fuelStrategyValues.rearBrakes,
+        [
+          Validators.required,
+          Validators.min(this.validateFuelStrategy.brakesMin),
+          Validators.max(this.validateFuelStrategy.brakesMax),
+        ]
+      ],
+    });
+  }
 }
