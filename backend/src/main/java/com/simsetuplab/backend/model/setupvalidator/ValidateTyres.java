2ba37399392133e3484e72559a93e0fe08707b55
lojb <balazsloj02@gmail.com>
2023-10-25 16:15:13 +0200

    Refactor ValidateTyres

--- backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateTyres.java
+++ backend/src/main/java/com/simsetuplab/backend/model/setupvalidator/ValidateTyres.java
@@ -3,11 +3,11 @@ package com.simsetuplab.backend.model.setupvalidator;
 import com.simsetuplab.backend.exception.ApiRequestException;
 import com.simsetuplab.backend.model.setup.setupvalues.Tyres;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,7 +28,10 @@ public class ValidateTyres {
 	private float casterMin;
 	private float casterMax;
 
-	public boolean validate(Tyres tyres) {
+	@Transient
+	List<String> errorList = new ArrayList<>();
+
+	public List<String> validate(Tyres tyres) {
 		validateTyrePressure(tyres.getFrontLeftTyrePressure());
 		validateTyrePressure(tyres.getFrontRightTyrePressure());
 		validateTyrePressure(tyres.getRearLeftTyrePressure());
@@ -39,47 +42,47 @@ public class ValidateTyres {
 		validateRearToe(tyres.getRearLeftToe(), tyres.getRearRightToe());
 		validateCaster(tyres.getCasterLeft(), tyres.getCasterRight());
 
-		return true;
+		return errorList;
 	}
 
 	private void validateTyrePressure(float tyrePressure) {
 		if (!(tyrePressure >= tyrePressureMin && tyrePressure <= tyrePressureMax)) {
-			throw new ApiRequestException("Invalid tyre pressure values");
+			errorList.add("tyre pressure");
 		}
 	}
 
 	private void validateFrontCamber(float frontLeftCamber, float frontRightCamber) {
 		if (!(frontLeftCamber >= frontCamberMin && frontLeftCamber <= frontCamberMax
 				&& frontRightCamber >= frontCamberMin && frontRightCamber <= frontCamberMax)) {
-			throw new ApiRequestException("Invalid front camber values");
+			errorList.add("front camber");
 		}
 	}
 
 	private void validateRearCamber(float rearLeftCamber, float rearRightCamber) {
 		if (!(rearLeftCamber >= rearCamberMin && rearLeftCamber <= rearCamberMax
 				&& rearRightCamber >= rearCamberMin && rearRightCamber <= rearCamberMax)) {
-			throw new ApiRequestException("Invalid rear camber values");
+			errorList.add("rear camber");
 		}
 	}
 
 	private void validateFrontToe(float frontLeftToe, float frontRightToe) {
 		if (!(frontLeftToe >= frontToeMin && frontLeftToe <= frontToeMax
 				&& frontRightToe >= frontToeMin && frontRightToe <= frontToeMax)) {
-			throw new ApiRequestException("Invalid front toe values");
+			errorList.add("front toe");
 		}
 	}
 
 	private void validateRearToe(float rearLeftToe, float rearRightToe) {
 		if (!(rearLeftToe >= rearToeMin && rearLeftToe <= rearToeMax
 				&& rearRightToe >= rearToeMin && rearRightToe <= rearToeMax)) {
-			throw new ApiRequestException("Invalid rear toe values");
+			errorList.add("rear toe");
 		}
 	}
 
 	private void validateCaster(float casterLeft, float casterRight) {
 		if (!(casterLeft >= casterMin && casterLeft <= casterMax
 				&& casterRight >= casterMin && casterRight <= casterMax)) {
-			throw new ApiRequestException("Invalid caster values");
+			errorList.add("caster");
 		}
 	}
 }
