0f8c700fb85e4ef7658c132f84cd9a8337cbc4f0
lojb <balazsloj02@gmail.com>
2023-10-31 12:58:52 +0100

    Add EventEmitter to aero.component.ts

--- frontend/src/app/setup/aero/aero.component.ts
+++ frontend/src/app/setup/aero/aero.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, NgModule} from '@angular/core';
+import {Component, EventEmitter, Input, NgModule, OnInit, Output} from '@angular/core';
 import {Aero} from "../../model/setup/aero";
 import {ValidateAero} from "../../model/validateSetup/validate-aero";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Tyres} from "../../model/setup/tyres";
 
 @Component({
   selector: 'app-aero',
@@ -9,16 +10,22 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./aero.component.css']
 })
 
-export class AeroComponent {
+export class AeroComponent implements OnInit{
 
   @Input() aeroValues: Aero;
   @Input() validateAero: ValidateAero;
+  @Output() dataUpdate = new EventEmitter<Aero>();
   aeroForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.initializeForm();
+
+    this.aeroForm.valueChanges.subscribe((formValue) => {
+      this.aeroValues = formValue;
+      this.dataUpdate.emit(this.aeroForm.value);
+    });
   }
 
   initializeForm() {
