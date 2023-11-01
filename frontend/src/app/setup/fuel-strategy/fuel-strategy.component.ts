5516bc1354aff7d42a5ee5e4bfee92376cabc66e
lojb <balazsloj02@gmail.com>
2023-11-01 12:59:43 +0100

    Add EventEmitter to fuel-strategy.component.ts

--- frontend/src/app/setup/fuel-strategy/fuel-strategy.component.ts
+++ frontend/src/app/setup/fuel-strategy/fuel-strategy.component.ts
@@ -1,22 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FuelStrategy} from "../../model/setup/fuel-strategy";
 import {ValidateFuelStrategy} from "../../model/validateSetup/validate-fuel-strategy";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Aero} from "../../model/setup/aero";
 
 @Component({
   selector: 'app-fuel-strategy',
   templateUrl: './fuel-strategy.component.html',
   styleUrls: ['./fuel-strategy.component.css']
 })
-export class FuelStrategyComponent{
+export class FuelStrategyComponent implements OnInit{
   @Input() fuelStrategyValues: FuelStrategy;
   @Input() validateFuelStrategy: ValidateFuelStrategy;
+  @Output() dataUpdate = new EventEmitter<FuelStrategy>();
   fuelStrategyForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.initializeForm();
+
+    this.fuelStrategyForm.valueChanges.subscribe((formValue) => {
+      this.fuelStrategyValues = formValue;
+      this.dataUpdate.emit(this.fuelStrategyForm.value);
+    });
   }
 
   initializeForm() {
