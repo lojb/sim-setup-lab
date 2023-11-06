ebe7f180c6df7686c9a43b88de766f18772dffc6
lojb <balazsloj02@gmail.com>
2023-11-06 17:26:25 +0100

    Implement ElectronicsJSON

---
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/ElectronicsJSON.java
@@ -0,0 +1,15 @@
+package com.simsetuplab.backend.model.jsonConverter.components.basicSetup;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ElectronicsJSON {
+    private int tC1;
+    private int tC2;
+    private int abs;
+    private int eCUMap;
+    private int fuelMix;
+    private int telemetryLaps;
+}
