5cd813b6a487fd9a5028bdf3c5b5cd9b574d8465
lojb <balazsloj02@gmail.com>
2023-11-06 17:25:41 +0100

    SetupJSON does not need to be an entity

--- backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/SetupJSON.java
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/SetupJSON.java
@@ -2,22 +2,15 @@ package com.simsetuplab.backend.model.jsonConverter;
 
 import com.simsetuplab.backend.model.jsonConverter.components.advancedSetup.AdvancedSetup;
 import com.simsetuplab.backend.model.jsonConverter.components.basicSetup.*;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+
 @Getter
 @Setter
-@Table(name = "setup_json", schema = "public")
 public class SetupJSON {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     String carName;
-    @OneToOne(cascade = CascadeType.ALL)
     BasicSetup basicSetup;
-    @OneToOne(cascade = CascadeType.ALL)
     AdvancedSetup advancedSetup;
     int trackBopType;
 }
