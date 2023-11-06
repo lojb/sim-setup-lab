31b7d58e05b9c784bd2d0eb22637d4cf43542f43
lojb <balazsloj02@gmail.com>
2023-11-06 17:26:18 +0100

    Implement DrivetrainJSON

---
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/advancedSetup/DrivetrainJSON.java
@@ -0,0 +1,10 @@
+package com.simsetuplab.backend.model.jsonConverter.components.advancedSetup;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DrivetrainJSON {
+    private int preload;
+}
