16c69485f1d7c80f4f1a12e52c607485b7ae7e3c
lojb <balazsloj02@gmail.com>
2023-11-08 09:39:22 +0100

    Add Builder annotation and comments to ElectronicsJSON

--- backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/ElectronicsJSON.java
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/ElectronicsJSON.java
@@ -1,15 +1,17 @@
 package com.simsetuplab.backend.model.jsonConverter.components.basicSetup;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ElectronicsJSON {
-    private int tC1;
-    private int tC2;
-    private int abs;
-    private int eCUMap;
-    private int fuelMix;
-    private int telemetryLaps;
+    private int tC1; // 1 click -> 1 unit
+    private int tC2; // 1 click -> 1 unit
+    private int abs; // 1 click -> 1 unit
+    private int eCUMap; // 1 click -> 1 unit
+    private int fuelMix; // porsche: always 0
+    private int telemetryLaps; // 1 click -> 1 lap
 }
