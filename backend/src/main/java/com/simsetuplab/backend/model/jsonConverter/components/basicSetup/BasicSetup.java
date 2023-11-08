16fae5c34ec924a7f5de387866ad1b0d6eecf536
lojb <balazsloj02@gmail.com>
2023-11-08 09:38:14 +0100

    Add Builder Annotation to BasicSetup

--- backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/BasicSetup.java
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/BasicSetup.java
@@ -1,10 +1,12 @@
 package com.simsetuplab.backend.model.jsonConverter.components.basicSetup;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class BasicSetup {
     private TyresJSON tyres;
     private AlignmentJSON alignment;
