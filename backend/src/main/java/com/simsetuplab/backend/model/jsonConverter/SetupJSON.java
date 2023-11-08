1f6329f47fa0ea42ba0a51c95eadeb36a526024f
lojb <balazsloj02@gmail.com>
2023-11-08 09:38:04 +0100

    Add Builder Annotation to SetupJSON

--- backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/SetupJSON.java
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/SetupJSON.java
@@ -2,12 +2,14 @@ package com.simsetuplab.backend.model.jsonConverter;
 
 import com.simsetuplab.backend.model.jsonConverter.components.advancedSetup.AdvancedSetup;
 import com.simsetuplab.backend.model.jsonConverter.components.basicSetup.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@Builder
 public class SetupJSON {
     String carName;
     BasicSetup basicSetup;
