36a5a527739bb1498467c3feab79f511cf8845d6
lojb <balazsloj02@gmail.com>
2023-11-08 09:39:07 +0100

    Add Builder annotation and comments to AlignmentJSON

--- backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/AlignmentJSON.java
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/basicSetup/AlignmentJSON.java
@@ -1,5 +1,6 @@
 package com.simsetuplab.backend.model.jsonConverter.components.basicSetup;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,12 +8,13 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class AlignmentJSON {
-    private List<Double> camber;
-    private List<Double> toe;
-    private List<Double> staticCamber;
-    private List<Double> toeOutLinear;
-    private double casterLF;
-    private double casterRF;
-    private double steerRatio;
+    private List<Double> camber; // 1 click -> 0.1° [ front left, front right, rear left, rear right ]
+    private List<Double> toe; // 1 click -> 0.01°  [ front left, front right, rear left, rear right ]
+    private List<Double> staticCamber; //calculated values
+    private List<Double> toeOutLinear; //calculated values
+    private double casterLF; // 1 click -> 0.2° except 7.7° + 1 click = 7.8°
+    private double casterRF; // 1 click -> 0.2° except 7.7° + 1 click = 7.8°
+    private double steerRatio; // 1 click -> 1.0°
 }
