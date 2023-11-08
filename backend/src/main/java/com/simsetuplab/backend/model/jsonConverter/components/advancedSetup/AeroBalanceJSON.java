8ef67ca0a1da27310d8ec26df1e12e29f990bf0b
lojb <balazsloj02@gmail.com>
2023-11-08 09:38:47 +0100

    Add Builder annotation and comments to AeroBalanceJSON

--- backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/advancedSetup/AeroBalanceJSON.java
+++ backend/src/main/java/com/simsetuplab/backend/model/jsonConverter/components/advancedSetup/AeroBalanceJSON.java
@@ -1,5 +1,6 @@
 package com.simsetuplab.backend.model.jsonConverter.components.advancedSetup;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +8,11 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class AeroBalanceJSON {
-    private List<Integer> rideHeight;
-    private List<Double> rodLength;
-    private int splitter;
-    private int rearWing;
-    private List<Integer> brakeDuct;
+    private List<Integer> rideHeight; // 1click -> 1 unit [ front, magic number1, rear, magic number2]
+    private List<Double> rodLength; // calculated value, can give 0 values
+    private int splitter; // 1click -> 1.0mm
+    private int rearWing; // 1click -> 1 unit
+    private List<Integer> brakeDuct; // [ front, rear ]
 }
