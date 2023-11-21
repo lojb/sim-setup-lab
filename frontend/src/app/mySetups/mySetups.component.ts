428c35c4f4e4182a335c24a5e314c8d9f9a9072e
lojb <balazsloj02@gmail.com>
2023-11-21 15:52:47 +0100

    Fetch all of users setups on init

---
+++ frontend/src/app/mySetups/mySetups.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {SetupValues} from "../model/setup/setup-values";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './mySetups.component.html',
+  styleUrls: ['./mySetups.component.css']
+})
+export class MySetupsComponent {
+
+  setupList: Array<SetupValues>;
+
+  constructor(private http: HttpClient) {
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+
+    this.http.get(`/api/v1/setup/user/${localStorage.getItem("userId")}`, {headers})
+      .subscribe((data: any) => {
+        this.setupList = data;
+        console.log(this.setupList);
+      })
+  }
+}
