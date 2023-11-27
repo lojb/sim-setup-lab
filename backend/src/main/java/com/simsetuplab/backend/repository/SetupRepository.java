14de232a0093cf142a6f56a76b511c09693590a6
lojb <balazsloj02@gmail.com>
2023-11-27 12:23:02 +0100

    Create method to get all custom setups by userId

--- backend/src/main/java/com/simsetuplab/backend/repository/SetupRepository.java
+++ backend/src/main/java/com/simsetuplab/backend/repository/SetupRepository.java
@@ -1,16 +1,20 @@
 package com.simsetuplab.backend.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.simsetuplab.backend.enumeration.carsetup.CarType;
 import com.simsetuplab.backend.enumeration.carsetup.SetupType;
 import com.simsetuplab.backend.enumeration.carsetup.Tracks;
 import com.simsetuplab.backend.model.setup.Setup;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SetupRepository extends JpaRepository<Setup, Long> {
-	Setup findSetupById(Long id);
+    Setup findSetupById(Long id);
 
-	List<Setup> findAllByUserId(Long userId);
+    List<Setup> findAllByUserId(Long userId);
 
-	Setup findSetupBySetupTypeAndTrackAndCarType(SetupType setupType, Tracks tracks, CarType carType);
+    Setup findSetupBySetupTypeAndTrackAndCarType(SetupType setupType, Tracks tracks, CarType carType);
+
+    List<Setup> findAllByUserIdAndSetupType(Long userId, SetupType setupType);
 }
