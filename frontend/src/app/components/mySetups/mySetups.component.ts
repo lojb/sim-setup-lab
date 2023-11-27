c228aad8ae62328e8520c589ade7546f72516c8b
lojb <balazsloj02@gmail.com>
2023-11-27 12:21:12 +0100

    Refactor MySetups component to use RequestService

--- frontend/src/app/components/mySetups/mySetups.component.ts
+++ frontend/src/app/components/mySetups/mySetups.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Component, inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 import {SetupValues} from "../../model/setup/setup-values";
+import {RequestService} from "../../service/request.service";
 
 @Component({
   selector: 'app-user',
@@ -11,17 +12,11 @@ export class MySetupsComponent {
 
   setupList: Array<SetupValues>;
 
-  constructor(private http: HttpClient) {
+  constructor(private requestService: RequestService) {
 
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('token')}`
-    });
-
-    this.http.get(`/api/v1/setup/user/${localStorage.getItem("userId")}`, {headers})
+    this.requestService.getCustomSetupsByUserId(Number(localStorage.getItem("userId")))
       .subscribe((data: any) => {
         this.setupList = data;
-        console.log(this.setupList);
       })
   }
 }
